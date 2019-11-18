066d40b98a56d9ead7ba4e1a6932b21c80fb4b3d
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-11-18 11:33:52 +0100

    Init with starting grammar

---
+++ LLVMCompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LLVMCompiler
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Init.");
+        }
+    }
+}
