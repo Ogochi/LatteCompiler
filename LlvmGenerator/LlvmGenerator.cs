52bd1e2334b9d8be36ed095bbb5f020f2be3e6e5
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-12-28 19:28:55 +0100

    Emiting external function declarations

--- LlvmGenerator/LlvmGenerator.cs
+++ LlvmGenerator/LlvmGenerator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Common.AST;
 
 namespace LlvmGenerator
@@ -16,14 +17,34 @@ namespace LlvmGenerator
             EmittedCode.Add(s);
         }
 
+        public void Emit(IList<string> s)
+        {
+            s.ToList().ForEach(Emit);
+        }
+
         public List<string> GenerateFromAst(Program program)
         {
-            Emit("");
+            EmitExternalFunctionsDeclarations();
+
             // TODO - generate
             
             var result = EmittedCode;
             EmittedCode = new List<string>();
             return result;
+        }
+
+        private void EmitExternalFunctionsDeclarations()
+        {
+            Emit(new List<string>
+            {
+                "declare void @printInt(i32)",
+                "declare void @printString(i8*)",
+                "declare void @error()",
+                "declare i32 @readInt()",
+                "declare i8* @readString()",
+                "declare i32 @strEq(i8*, i8*)",
+                "declare i8* @strConcat(i8*, i8*)"
+            });
         }
     }
 }
